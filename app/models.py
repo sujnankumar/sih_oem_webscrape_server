429c3795a2553b8002a8c09670b882045f3af432
sujalmh9@gmail.com <sujalmh>
2024-12-11 23:33:27 +0530

    merge

--- app/models.py
+++ app/models.py
@@ -116,8 +116,6 @@ class Vulnerabilities(db.Model):
     oem_website_id = db.Column(db.Integer, db.ForeignKey('oem_websites.id'), nullable=False)
     oem_website = db.relationship('OEMWebsite', backref=db.backref('vulnerabilities', lazy=True))
     additional_details = db.Column(db.JSON, nullable=True)
-
-    def __init__(self, product_name_version, oem_name, severity_level, vulnerability, remediation, published_date, unique_id, scraped_date, oem_website_id, additional_details=None):
         
     def __init__(
         self,
